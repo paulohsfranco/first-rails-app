12e2084236dfbc71d80cc15abb4043b164382c40
Paulo Franco <paulohenriquefranco@gmail.com>
2021-10-19 17:10:12 -0300

    teste

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+# VERB URL     controller#action -> método no controller
+  root to: 'pages#home'
+  get 'about', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
 end
